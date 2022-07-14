156062f785009b8bc4030564f185b6263610616c
Ali-0mar <ali.o@storexweb.com>
2022-07-14 18:22:01 +0300

    newFix

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const https = require('https');
+const http = require('http');
 const fs = require("fs");
 const path = require("path");
 const io = require('socket.io');
 
-const options = {
-	key: fs.readFileSync(path.join(__dirname, "public", "key.pem")),
-	cert: fs.readFileSync(path.join(__dirname, "public", "cert.pem"))
-};
+//const options = {
+//	key: fs.readFileSync(path.join(__dirname, "public", "key.pem")),
+//	cert: fs.readFileSync(path.join(__dirname, "public", "cert.pem"))
+//};
 const apiServer = require('./api');
-const httpServer = https.createServer(options, apiServer);
+const httpServer = http.createServer(apiServer);
 const socketServer = io(httpServer);
 
 const sockets = require('./sockets');
 
 const PORT = 3000;
-httpServer.listen(PORT);
-console.log(`Listening on port ${PORT}...`);
-
-sockets.listen(socketServer);
-;
+httpServer.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`);
+	sockets.listen(socketServer);
+});
