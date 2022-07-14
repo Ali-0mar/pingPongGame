8f86795edfb251d3a99efb80757c98a652dbcb17
Ali-0mar <ali.o@storexweb.com>
2022-07-14 17:27:42 +0300

    fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const https = require('https');
 const fs = require("fs");
+const path = require("path");
 const io = require('socket.io');
 
 const options = {
-	key: fs.readFileSync("key.pem"),
-	cert: fs.readFileSync("cert.pem")
+	key: fs.readFileSync(path.join(__dirname, "public", "key.pem")),
+	cert: fs.readFileSync(path.join(__dirname, "public", "cert.pem"))
 };
 const apiServer = require('./api');
 const httpServer = https.createServer(options, apiServer);
